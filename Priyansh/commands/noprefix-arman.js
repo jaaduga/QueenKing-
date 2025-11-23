92f4b07f209ad45758f2d16f264dcd71f457dc8f
jaaduga <bavdinjuneja.7566@gmail.com>
2025-11-23 13:53:53 +0530

    Update noprefix-arman.js

--- Priyansh/commands/noprefix-arman.js
+++ Priyansh/commands/noprefix-arman.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "arman",
+	name: "umar",
     version: "1.0.1",
 	hasPermssion: 0,
 	credits: "MrTomXxX", 
@@ -12,10 +12,10 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("@Arman")==0 || event.body.indexOf("Arman")==0 || event.body.indexOf("ARMAN")==0 || event.body.indexOf("arman")==0) {
+	if (event.body.indexOf("@Umar Malik")==0 || event.body.indexOf("UMAR")==0 || event.body.indexOf("umar")==0 || event.body.indexOf("Umariya")==0) {
 		var msg = {
-				body: "𝗛𝗲 𝗶𝘀 𝗕𝗲𝘀𝘁 𝗙𝗿𝗶𝗲𝗻𝗱 𝗢𝗳 𝗠𝘆 𝗕𝗼𝘀𝘀 𝗳𝗮𝗿𝗮𝘇 \n 💞 \n 🌹𝙰𝚛𝚖𝚊𝚗 𝙼𝚊𝚕𝚒𝚔😘♥️🌹",
-				attachment: fs.createReadStream(__dirname + `/noprefix/arman.jpg`)
+				body: "𝗛𝗲 𝗶𝘀 𝗕𝗲𝘀𝘁 𝗙𝗿𝗶𝗲𝗻𝗱 𝗢𝗳 𝗠𝘆 𝗕𝗼𝘀𝘀 𝗳𝗮𝗿𝗮𝘇 \n 💞 \n 🌹Umar Malik 😘♥️🌹",
+				attachment: fs.createReadStream(__dirname + `/noprefix/uamar.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🍿", event.messageID, (err) => {}, true)
