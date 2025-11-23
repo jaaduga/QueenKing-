5a418a70d58ea260943ec4b2599c853c2e3cc742
jaaduga <bavdinjuneja.7566@gmail.com>
2025-11-23 14:05:04 +0530

    Update noprefix-mahadev.js

--- Priyansh/commands/noprefix-mahadev.js
+++ Priyansh/commands/noprefix-mahadev.js
@@ -12,10 +12,10 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Maha")==0 || event.body.indexOf("Har har")==0 || event.body.indexOf("Bhole")==0 || event.body.indexOf("Shiv")==0) {
+	if (event.body.indexOf("har har mahadev")==0 || event.body.indexOf("Har har Mahadev")==0 || event.body.indexOf("Bhole")==0 || event.body.indexOf("Shiv")==0) {
 		var msg = {
 				body: "𝙷𝚊𝚛 𝚑𝚊𝚛 𝚖𝚊𝚑𝚊𝚍𝚎𝚟♥️ 𝙹𝚊𝚒 𝚖𝚊𝚑𝚊𝚔𝚊𝚕🌹",
-				attachment: fs.createReadStream(__dirname + `/noprefix/mahakal.mp4`)
+				attachment: fs.createReadStream(__dirname + `/noprefix/mahadev.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🔱", event.messageID, (err) => {}, true)
